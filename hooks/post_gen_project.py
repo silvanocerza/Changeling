966dec05096af58937847a6b20e14f1b564a6a19
Silvano Cerza <silvanocerza@gmail.com>
2018-06-29 10:10:45 +0200

    Fixed hooks typo

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,10 +4,10 @@ import shutil
 
 def main():
     if "{{cookiecutter.use_django_rest_framework}}".lower() == "y":
-        shutil.rmtree(os.path.join("django-{{cookicutter.project_slug}}", "api"))
+        shutil.rmtree(os.path.join("django-{{cookiecutter.project_slug}}", "api"))
 
     if "{{cookiecutter.add_frontend}}".lower() == "y":
-        shutil.rmtree(os.path.join("django-{{cookicutter.project_slug}}", "frontend"))
+        shutil.rmtree(os.path.join("django-{{cookiecutter.project_slug}}", "frontend"))
 
     if "{{cookiecutter.license}}" != "GPLv3":
         os.remove("COPYING")
